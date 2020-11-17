9f88bdd2e98487f1fc4200afb8e2e5301324dc55
Omar Msaaf <omar.msaaf@3dsemantix.com>
2020-11-17 18:06:40 -0500

    Première itération dans Github Code permettant de récupérer les faces dans CATIA et dans SolidWorks à partir d'un assemblage

---
+++ src/PlaneFacesArea/CATIAPlaneFacesArea/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HybridShapeTypeLib;
+using INFITF;
+using MECMOD;
+using ProductStructureTypeLib;
+using SPATypeLib;
+
+namespace CATIAPlaneFacesArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var catiaType = Type.GetTypeFromProgID("CATIA.Application");
+            var catiaInstane = (Application)Activator.CreateInstance(catiaType);
+            var activeDocument = catiaInstane.ActiveDocument;
+            if (activeDocument == null)
+            {
+                Console.WriteLine("Veuillez ouvrir un assemblage.");
+                Console.ReadKey();
+                return;
+            }
+            else if (! (activeDocument is ProductDocument))
+            {
+                Console.WriteLine("Veuillez ouvrir un assemblage.");
+                Console.ReadKey();
+                return;
+            }
+
+            var productDocument = (ProductDocument)activeDocument;
+            var selection = productDocument.Selection;
+
+            var rootProduct = productDocument.Product;
+            var products = rootProduct.Products;
+            var workbenchId = catiaInstane.GetWorkbenchId();
+            var areas = 0.0;
+            for (int i = 1; i <= products.Count; i++)
+            {
+
+                var product = products.Item(i);
+                var instanceName = product.get_Name();
+                var masterShape = product.GetMasterShapeRepresentation(true);
+                var partDocumentOf = masterShape as PartDocument;
+
+
+                var documentName = product.GetMasterShapeRepresentationPathName();
+                Console.WriteLine($"Processing document {documentName}");
+
+                selection.Clear();
+                selection.Add(partDocumentOf.Part);
+
+
+                catiaInstane.StartWorkbench("PrtCfg");
+                workbenchId = catiaInstane.GetWorkbenchId();
+
+                var part = partDocumentOf.Part;
+                var partSelection = partDocumentOf.Selection;
+                partSelection.Clear();
+                partSelection.Add(part.MainBody);
+
+
+                var hybridBody = part.HybridBodies.Add();
+                var hsf = (HybridShapeFactory)part.HybridShapeFactory;
+
+                selection.Search("Topology.CGMFace,sel");
+                var extracts = new List<HybridShapeExtract>();
+                for (var selectedIndex = 1; selectedIndex <= selection.Count2; selectedIndex++)
+                {
+                    var selectedItem = selection.Item2(selectedIndex).Reference;
+                    var extract = hsf.AddNewExtract(selectedItem);
+                    extracts.Add(extract);
+                    hybridBody.AppendHybridShape(extract);
+                }
+                part.Update();
+                catiaInstane.StartWorkbench("Inertia");
+                var spaWb = (SPAWorkbench)partDocumentOf.GetWorkbench("SPAWorkbench");
+               
+                foreach (var extract in extracts)
+                {
+                    try
+                    {
+
+                        var measurableOnExtract = spaWb.GetMeasurable(part.CreateReferenceFromObject(extract));
+                        Console.WriteLine(measurableOnExtract.GeometryName);
+                        var planecomponents = new object[9]; //new object[9];//Enumerable.Range(0,9).Select(x=> 0.0).ToArray();
+                        if (measurableOnExtract.GeometryName == CatMeasurableName.CatMeasurablePlane)
+                        {
+                            areas += measurableOnExtract.Area;
+
+                            measurableOnExtract.GetPlane(planecomponents);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                    }
+                }
+
+                selection.Clear();
+                selection.Add(rootProduct);
+
+                catiaInstane.StartWorkbench("Assembly");
+
+                workbenchId = catiaInstane.GetWorkbenchId();
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ src/PlaneFacesArea/PlaneFacesArea/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using SldWorks;
+using SwConst;
+
+namespace PlaneFacesArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SldWorks.SldWorks swApp;
+            ModelDoc2 swModelDoc;
+            AssemblyDoc swAssemblyDoc;
+            Object[] components;
+            double totalAreaPlanarFaces = 0;
+
+            swApp = new SldWorks.SldWorks();
+            swApp.Visible = true;
+            swModelDoc = (ModelDoc2)swApp.ActiveDoc;
+            if (swModelDoc == null)
+            {
+                Console.WriteLine("Veuillez ouvrir un assemblage.");
+                Console.ReadKey();
+                return;
+            }
+            else if (swModelDoc.GetType() != (int)swDocumentTypes_e.swDocASSEMBLY)
+            {
+                Console.WriteLine("Veuillez ouvrir un assemblage.");
+                Console.ReadKey();
+                return;
+            }
+            swAssemblyDoc = (AssemblyDoc)swModelDoc;
+            components = (object[])swAssemblyDoc.GetComponents(false);
+            foreach (var item in components)
+            {
+                Component2 swComponent;
+                swComponent = (Component2)item;
+                Console.WriteLine(swComponent.Name);
+                if (swComponent.IsSuppressed() == true)
+                {
+                    continue;
+                }
+                else
+                {
+                    ModelDoc2 swComponentDoc;
+                    swComponentDoc = (ModelDoc2)swComponent.GetModelDoc2(); // Connexion au document (pièce ou assemblage) lié au composant.
+                    if (swComponentDoc == null)
+                    {
+                        // Le ModelDoc2 sera Nothing si le fichier lié au composant n'est pas ouvert en mémoire(lightweight, supprimé, fichier non trouvé, etc.).
+                        continue;
+                    }
+                    // On doit s'assurer que ce composant est lié à une pièce et non à un assemblage. Sinon, le cast vers le type PartDoc créera un crash(une exception).
+                    if (swComponentDoc.GetType() == (int)swDocumentTypes_e.swDocPART)
+                    {
+                        PartDoc swCompPartDoc;
+                        double PlanarFacesAreaForPart = 0;
+                        swCompPartDoc = (PartDoc)swComponentDoc;
+                        PlanarFacesAreaForPart = GetAreaOfPlanarFacesOfPart(swCompPartDoc);
+                        totalAreaPlanarFaces += PlanarFacesAreaForPart;
+                        Console.WriteLine("Aire totale temporaire = {0}", totalAreaPlanarFaces);
+                    }
+                }
+            }
+            Console.WriteLine("L'aire de toutes les faces planes vaut: {0}", totalAreaPlanarFaces);
+            Console.ReadKey();
+        }
+        static double GetAreaOfPlanarFacesOfPart(PartDoc _swPartDoc)
+        {
+            double area = 0;
+            Surface swSurface;
+            Face2 swFace;
+            Body2 swBody;
+            Object[] bodies;
+            object[] faces;
+
+            bodies = (object[])_swPartDoc.GetBodies2((int)swBodyType_e.swSolidBody, true);
+            for (int i = 0; i < bodies.Length; i++)
+            {
+                Object objBody;
+                objBody = bodies[i];
+                swBody = (Body2)objBody;
+                faces = (object[])swBody.GetFaces();
+                for (int j = 0; j < faces.Length; j++)
+                {
+                    Object objFace;
+                    objFace = faces[j];
+                    swFace = (Face2)objFace;
+                    swSurface = (Surface)swFace.GetSurface();
+                    if (swSurface.IsPlane())
+                    {
+                        // obtenir l'aire et l'additionner au reste
+                        area += swFace.GetArea();
+                        Console.WriteLine("Aire de la face = {0}", area);
+                    }
+                }
+            }
+            return area;
+        }
+    }
+}
